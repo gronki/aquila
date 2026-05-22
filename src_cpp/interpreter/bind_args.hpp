557bbccf85f7950e3cdd64155d8809231486ae35
Dominik Gronkiewicz <gronki@gmail.com>
2026-05-22 06:22:27 +0200

    now bind_args can handle ellipsis!

--- src_cpp/interpreter/bind_args.hpp
+++ src_cpp/interpreter/bind_args.hpp
@@ -2,6 +2,7 @@
 
 #include <cstdint>
 #include <memory>
+#include <variant>
 #include <vector>
 
 #include "value.hpp"
@@ -10,48 +11,111 @@ namespace aquila::interpreter
 {
 
 template <typename T>
-struct __handler {
-    static const T* cast(const Value* v) { return value_cast<T>(v); }
-    static const value_type& type_name() { return T::type_name; }
+struct __handler
+{
+    static const T *cast(const Value *v) { return value_cast<T>(v); }
+    static const value_type &type_name() { return T::type_name; }
 };
 template <>
-struct __handler<Value> {
+struct __handler<Value>
+{
     static constexpr value_type any_value{"any"};
-    static const Value* cast(const Value* v) { return v; }
-    static const value_type& type_name() { return any_value; }
+    static const Value *cast(const Value *v) { return v; }
+    static const value_type &type_name() { return any_value; }
 };
 template <typename T>
-struct __sv_handler {
-    static const T* cast(const Value* v) { 
-            auto sv = value_cast<SimpleValue<T>>(v); 
-            return sv ? &sv->value : nullptr; }
-    static const value_type& type_name() { return SimpleValue<T>::type_name; }
+struct __sv_handler
+{
+    static const T *cast(const Value *v)
+    {
+        auto sv = value_cast<SimpleValue<T>>(v);
+        return sv ? &sv->value : nullptr;
+    }
+    static const value_type &type_name() { return SimpleValue<T>::type_name; }
 };
-template<> struct __handler<double> : __sv_handler<double> {};
-template<> struct __handler<std::int64_t> : __sv_handler<std::int64_t> {};
-template<> struct __handler<std::string> : __sv_handler<std::string> {};
+template <>
+struct __handler<double> : __sv_handler<double>
+{
+};
+template <>
+struct __handler<std::int64_t> : __sv_handler<std::int64_t>
+{
+};
+template <>
+struct __handler<std::string> : __sv_handler<std::string>
+{
+};
 
 template <typename T>
-const T &__cast_one(const std::vector<const Value *> &args, std::size_t idx)
+struct vector_or
 {
-    const Value* arg = args[idx];
-    if (!arg)
+};
+template <typename T>
+struct vector_or<std::vector<const T *>>
+{
+    std::vector<const T *> vec;
+};
+
+struct __vector_holder_base
+{
+};
+
+template <typename T>
+struct __caster
+{
+    static constexpr std::size_t count_arg = 1;
+    const T &cast(const std::vector<const Value *> &args, std::size_t idx)
     {
-        throw std::runtime_error(
-            std::string("Nul input argument ") + std::to_string(idx + 1));
+        const Value *arg = args[idx];
+        if (!arg)
+        {
+            throw std::runtime_error(
+                std::string("Nul input argument ") + std::to_string(idx + 1));
+        }
+        const T *tptr = __handler<T>::cast(arg);
+        // cast worked
+        if (tptr)
+        {
+            return *tptr;
+        }
+        // wrong cast
+        throw std::runtime_error(std::string("Error trying to interpret "
+                                             "argument ")
+            + std::to_string(idx + 1) + ": expected " + __handler<T>::type_name().str()
+            + " but got " + arg->get_type().str() + " " + arg->str());
     }
-    const T *tptr = __handler<T>::cast(arg);
-    // cast worked
-    if (tptr)
+};
+
+template <typename T>
+struct __caster<std::vector<const T *>>
+{
+    std::vector<const T *> ptrs;
+    static constexpr std::size_t count_arg = 0;
+    const std::vector<const T *> &cast(
+        const std::vector<const Value *> &args, std::size_t idx)
     {
-        return *tptr;
+        ptrs.reserve(idx <= args.size() ? args.size() - idx : 0);
+        for (auto iarg = idx; iarg < args.size(); iarg++)
+        {
+            const Value *arg = args[iarg];
+            if (!arg)
+            {
+                throw std::runtime_error(
+                    std::string("Nul input argument ") + std::to_string(iarg + 1));
+            }
+            const T *tptr = __handler<T>::cast(arg);
+            if (!tptr)
+                // wrong cast
+                throw std::runtime_error(std::string("Error trying to interpret "
+                                                     "argument ")
+                    + std::to_string(iarg + 1) + ": expected "
+                    + __handler<T>::type_name().str() + " but got "
+                    + arg->get_type().str() + " " + arg->str());
+            ptrs.push_back(tptr);
+        }
+        return ptrs;
     }
-    // wrong cast
-    throw std::runtime_error(std::string("Error trying to interpret "
-                                         "argument ")
-        + std::to_string(idx + 1) + ": expected " + __handler<T>::type_name().str()
-        + " but got " + arg->get_type().str() + " " + arg->str());
-}
+};
 
 template <typename OpT, typename... ArgsT, std::size_t... iarg>
 inline std::unique_ptr<Value> __bind_args(const OpT *obj,
@@ -59,7 +123,15 @@ inline std::unique_ptr<Value> __bind_args(const OpT *obj,
     const std::vector<const Value *> &args,
     std::index_sequence<iarg...>)
 {
-    return (obj->*exec_fun)(__cast_one<ArgsT>(args, iarg)...);
+    std::tuple<__caster<ArgsT>...> casters;
+    auto min_args = ((std::get<iarg>(casters).count_arg) + ... + std::size_t(0));
+    bool has_vector = sizeof...(ArgsT) > min_args;
+    if (args.size() < min_args || (!has_vector && args.size() != sizeof...(ArgsT)))
+        throw std::runtime_error(
+            std::string("Argument list length incorrect: expected minimum ")
+            + std::to_string(min_args) + " arguments but got "
+            + std::to_string(args.size()));
+    return (obj->*exec_fun)(std::get<iarg>(casters).cast(args, iarg)...);
 }
 
 template <typename OpT, typename... ArgsT>
@@ -67,14 +139,6 @@ inline std::unique_ptr<Value> bind_args(const OpT *obj,
     std::unique_ptr<Value> (OpT::*exec_fun)(const ArgsT &...) const,
     const std::vector<const Value *> &args)
 {
-    if (sizeof...(ArgsT) != args.size())
-    {
-        throw std::runtime_error(
-            std::string("Argument list length incorrect: expected ")
-            + std::to_string(sizeof...(ArgsT)) + " arguments but got "
-            + std::to_string(args.size()));
-    }
-
     return __bind_args(obj, exec_fun, args, std::index_sequence_for<ArgsT...>{});
 }
 
---
+++ src_cpp/lib/operations/example/testarg.cpp
@@ -0,0 +1,43 @@
+#ifndef NDEBUG
+
+#    include <iostream>
+#    include <memory>
+#    include <operation.hpp>
+#    include <value.hpp>
+
+using namespace aquila;
+class TestOperation : public Operation
+{
+public:
+    std::string name() const override { return "testing"; }
+    interpreter::ArgManifest arg_manifest() const override
+    {
+        return interpreter::ArgManifest{
+            ArgSpec{.name = "pos1"},
+            ArgSpec{.name = "pos2"},
+            ArgSpec{.name = "key1", .default_str = "key1"},
+            ArgSpec{.name = "key2", .default_str = "key2"},
+            ArgSpec{.name = "..."},
+        };
+    }
+    interpreter::ValuePtr run(const Str &pos1,
+        const Str &pos2,
+        const Str &key1,
+        const Str &key2,
+        const std::vector<const Str *> &ellipsis) const
+    {
+        std::cout << "pos1 = " << pos1 << std::endl
+                  << "pos2 = " << pos2 << std::endl
+                  << "key1 = " << key1 << std::endl
+                  << "key2 = " << key2 << std::endl;
+        for (auto strptr : ellipsis)
+        {
+            std::cout << " ... " << *strptr << std::endl;
+        }
+        return std::make_unique<StrValue>("hello");
+    }
+    BIND_ARGS(&TestOperation::run);
+};
+REGISTER(TestOperation);
+
+#endif
